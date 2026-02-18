892b67058329671e3adc5c3a7c5131c14523e781
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-02-18 23:43:33 +0500

    feat(index): configure app entry point and middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,24 @@
 /** Entry point */
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+const port = ENV.PORT;
+
+/** Middlewares */
+app.use(cors({ origin: ENV.FRONTEND_URL })); // Enables CORS for all routes
+app.use(clerkMiddleware()); // Integrates Clerk authentication middleware
+app.use(express.json()); // Parses incoming JSON requests
+app.use(express.urlencoded({ extended: true })); // Parses form data
+
+//** Routes */
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Welcome to ShopSphere API!" });
+});
+
+//** Start the server */
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
